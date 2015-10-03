96eb8c0bf8040483900f27518afd5d31f94b7e24
Zen Savona <z@zensavona.com>
2015-10-03 16:16:14 +0200

    add callback url and pages

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,25 @@ defmodule InstagramPhoenixExample.PageController do
   use InstagramPhoenixExample.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    if get_session(conn, :access_token) do
+      user = Elixtagram.user(:self, :global)
+      media = Elixtagram.user_recent_media(:self, %{count: 12})
+      data = %{ user: user, media: media, is_authenticated: true }
+    else
+      Elixtagram.configure
+      data = %{ url: Elixtagram.authorize_url!, is_authenticated: false }
+    end
+
+    render conn, "index.html", data: data
+  end
+
+  def callback(conn, %{"code" => code}) do
+    access_token = Elixtagram.get_token!(code: code).access_token
+    Elixtagram.configure(:global, access_token)
+
+    conn
+    |> put_session(:access_token, access_token)
+    |> configure_session(renew: true)
+    |> redirect(to: page_path(conn, :index))
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule InstagramPhoenixExample.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/callback", PageController, :callback
   end
 
   # Other scopes may use custom stacks.
