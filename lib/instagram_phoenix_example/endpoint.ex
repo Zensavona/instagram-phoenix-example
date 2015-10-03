3bce7e5217b340e35ede711be86c879e0cd087a6
Zen Savona <z@zensavona.com>
2015-10-03 15:25:52 +0200

    serve fonts

--- lib/instagram_phoenix_example/endpoint.ex
+++ lib/instagram_phoenix_example/endpoint.ex
@@ -9,7 +9,7 @@ defmodule InstagramPhoenixExample.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :instagram_phoenix_example, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images fonts js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
